cf763b2572d9f94968420362338c9959fc2795db
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-12-20 16:29:20 +0100

    no message

--- src/EasyBlogBundle.php
+++ src/EasyBlogBundle.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EasyBlogBundle extends Bundle
 {
+    /**
+     * @return ExtensionInterface|null The container extension
+     */
     public function getContainerExtension()
     {
         return new EasyBlogExtension();
